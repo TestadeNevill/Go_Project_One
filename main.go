ca53a5c63f236b203e9632eaa8ce24ea541c59de
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2022-04-01 13:25:30 -0400

    Collecting user input with Scan. Displaying final message with user input added in terminal

--- main.go
+++ main.go
@@ -3,5 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World ")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend!")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	// ask user for name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for book %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
 }
