e3e55ed55c546889b60e637d3272e6100ac142de
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2022-04-13 14:25:54 -0400

    Practicing Go Concurrency

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 // variables shared to main and all functions are package level variables.
 //Define variables as LOCAL as posssible
@@ -18,40 +22,44 @@ type UserData struct {
 	numberOfTickets uint
 }
 
+var wg = sync.WaitGroup{}
+
 func main() {
 
 	greetUsers()
 
-	for {
+	firstName, lastName, email, userTickets := getUserInput()
+	isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
-		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+	if isValidName && isValidEmail && isValidTicketNumber {
+
 		bookTicket(userTickets, firstName, lastName, email)
 
-		if isValidName && isValidEmail && isValidTicketNumber {
+		wg.Add(1)
+		go sendTicket(userTickets, firstName, lastName, email)
 
-			firstNames := getFirstNames()
-			fmt.Printf("The first name(s) of our booked guest(s) %v\n", firstNames)
+		firstNames := getFirstNames()
+		fmt.Printf("The first name(s) of our booked guest(s) %v\n", firstNames)
 
-			if remainingTickets == 0 {
-				// end program
-				fmt.Println("SOLD OUT, MAYBE NEXT TIME!")
-				break
-			}
+		if remainingTickets == 0 {
+			// end program
+			fmt.Println("SOLD OUT, MAYBE NEXT TIME!")
 
-		} else {
+		}
 
-			if !isValidName {
-				fmt.Printf("The first or last name you entered is too short. Please Try Again. \n")
-			}
-			if !isValidEmail {
-				fmt.Printf("Email address does not contain an @ sign. Please Try Again. \n")
-			}
-			if !isValidTicketNumber {
-				fmt.Printf("The number of tickets you entered is invalid. Please Try Again. \n")
-			}
+	} else {
+
+		if !isValidName {
+			fmt.Printf("The first or last name you entered is too short. Please Try Again. \n")
+		}
+		if !isValidEmail {
+			fmt.Printf("Email address does not contain an @ sign. Please Try Again. \n")
+		}
+		if !isValidTicketNumber {
+			fmt.Printf("The number of tickets you entered is invalid. Please Try Again. \n")
 		}
 	}
+	wg.Wait()
 }
 
 func greetUsers() {
@@ -111,4 +119,13 @@ func bookTicket(userTickets uint, firstName string, lastName string, email strin
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+}
+
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(50 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets for %v %v\n", userTickets, firstName, lastName)
+	fmt.Println("#######################")
+	fmt.Printf("Sending ticket:\n %v \nto email address %v \n", ticket, email)
+	fmt.Println("#######################")
+	wg.Done()
 }
