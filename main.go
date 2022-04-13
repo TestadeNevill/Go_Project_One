80e4a93b9a4b720f4a94c3f26e0c11440329dad2
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2022-04-13 13:32:20 -0400

    turned bookings list from slice of strings to slice of maps

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
-
 import (
 	"fmt"
-	"strings"
-	"booking-app/helper"
+	"strconv"
 )
 
 // variables shared to main and all functions are package level variables.
@@ -14,7 +12,7 @@ const conferenceTickets int = 50
 
 var conferenceName = "Go Conference"
 var remainingTickets uint = 50
-var bookings = []string{}
+var bookings = make([]map[string]string, 0)
 
 func main() {
 
@@ -23,7 +21,7 @@ func main() {
 	for {
 
 		firstName, lastName, email, userTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 		bookTicket(userTickets, firstName, lastName, email)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
@@ -61,13 +59,11 @@ func greetUsers() {
 func getFirstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		var names = strings.Fields(booking)
-		firstNames = append(firstNames, names[0])
+		firstNames = append(firstNames, booking["firstName"])
 	}
 	return firstNames
 
 }
-
 
 // no parameters because user input
 func getUserInput() (string, string, string, uint) {
@@ -93,7 +89,20 @@ func getUserInput() (string, string, string, uint) {
 
 func bookTicket(userTickets uint, firstName string, lastName string, email string) {
 	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+
+	// create map for user
+	// define key value string int ext but must have same value
+	// convert uint to string using strconv.FormatUint(uint64())
+
+	var userData = make(map[string]string)
+	userData["firstName"] = firstName
+	userData["lastName"] = lastName
+	userData["email"] = email
+	userData["numberOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+
+	// Turn bookings list from slice of strings to slice of maps
+	bookings = append(bookings, userData)
+	fmt.Printf("List of bookings is %v\n", bookings)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
